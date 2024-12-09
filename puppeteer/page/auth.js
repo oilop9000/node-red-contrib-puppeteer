cde09b4f3b8b1260af9150206639590e8528c03e
oilop9000 <oilop9000@gmail.com>
2024-12-09 00:54:19 -0600

    http authentication working, added socks5 proxy support

--- puppeteer/page/auth.js
+++ puppeteer/page/auth.js
@@ -3,31 +3,30 @@ module.exports = function (RED) {
   function PuppeteerHttpAuth(nodeConfig) {
     RED.nodes.createNode(this, nodeConfig);
     var node = this; // Referencing the current node
-    var username = this.credentials.username;
-    var password = this.credentials.password;
-    // Retrieve the config node
+    // Get Credentials
+    var credentials = this.credentials; 
     this.on("input", async function (msg, send, done) {
       try {
         // Get Authentication parameters
-        if (!nodeConfig.username || !nodeConfig.password) {
+        var username = msg._credentials.user || credentials.user;
+        var password = msg._credentials.password || credentials.password;
+        if (!username || !password) {
           throw new Error("Missing username or password");
         }
-        
         // Authenticate
-        node.status({ fill: "blue", shape: "dot", text: `Authenticating ${credentials.username}...` });
-        await msg.puppeteer.page.authenticate(credentials);
+        node.status({ fill: "blue", shape: "dot", text: `Authenticating ${username}...` });
+        await msg.puppeteer.page.authenticate({username, password});
         // Send the updated msg
         send(msg);
       } catch (e) {
-        // If an error occured
-        node.error(e);
         // Update the status
-        node.status({ fill: "red", shape: "dot", text: e });
+        node.status({ fill: "red", shape: "dot", text: e.message });
+        // If an error occured
+        done(e);
         // And update the message error property
-        msg.error = e;
-        send(msg);
+        msg.error = e.message; 
+        send(msg);f
       }
-
       // Clear status of the node
       setTimeout(
         () => {
@@ -41,7 +40,8 @@ module.exports = function (RED) {
       node.status({});
     });
     oneditprepare: function oneditprepare() {
-      $("#node-input-user").val(nodeConfig.user);
+      $("#node-input-name").val(nodeConfig.name);
+      $("#node-input-username").val(nodeConfig.username);
       $("#node-input-password").val(nodeConfig.password);
     }
   }
